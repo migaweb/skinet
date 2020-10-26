ee9cabc7012c766e8eb51afa5ae3cc467354e9da
micke <miga02@gmail.com>
2020-10-26 19:48:39 +0100

    Added routing

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -7,6 +7,7 @@ import { IPagination } from '../shared/models/pagination';
 import { IBrand } from '../shared/models/brand';
 import { IType } from '../shared/models/productType';
 import { ShopParams } from '../shared/models/shopParams';
+import { IProduct } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root'
@@ -43,6 +44,10 @@ export class ShopService {
     //       return response.body;
     //     })
     //   );
+  }
+
+  getProduct(id: number): Observable<IProduct> {
+    return this.http.get<IProduct>(`${this.baseUrl}products/${id}`);
   }
 
   getTypes(): Observable<IType[]> {
