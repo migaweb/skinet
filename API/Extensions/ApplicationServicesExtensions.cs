a52c249e43490e452e2c8b31e3cf9494f1559b53
micke <miga02@gmail.com>
2020-10-29 20:04:48 +0100

    Added redis and basket repo

--- API/Extensions/ApplicationServicesExtensions.cs
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -12,6 +12,7 @@ namespace API.Extensions
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
       services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+      services.AddScoped<IBasketRepository, BasketRepository>();
       services.AddScoped<IProductRepository, ProductRepository>();
 
       services.Configure<ApiBehaviorOptions>(options =>
