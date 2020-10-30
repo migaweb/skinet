b97bf30af1a5149b8a882a125226334ff3b06897
micke <miga02@gmail.com>
2020-10-30 20:24:01 +0100

    Added basket

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -12,6 +12,7 @@ const routes: Routes = [
   { path: 'server-error', component: ServerErrorComponent, data: {breadcrumb: 'Server Error'} },
   { path: 'not-found', component: NotFoundComponent, data: {breadcrumb: 'Not found'} },
   { path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule), data: {breadcrumb: 'Shop'} },
+  { path: 'basket', loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule), data: {breadcrumb: 'Basket'} },
   { path: '**', redirectTo: 'not-found', pathMatch: 'full' }
 ];
 
--- client/src/app/core/interceptors/loading.interceptors.ts
+++ client/src/app/core/interceptors/loading.interceptors.ts
@@ -11,7 +11,6 @@ export class LoadingInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     this.busyService.busy();
     return next.handle(req).pipe(
-      delay(1000),
       finalize(() => { this.busyService.idle(); })
     );
   }
